bc9ec6f3a18177f3c22f6fcccdf31b8b1aaa31c8
linghongren <lhng.rn@gmail.com>
2015-02-18 17:13:51 -0500

    separate out Map Activity from main activity

--- pathogion/app/src/main/java/com/example/ugri/pathogion/MainActivity.java
+++ pathogion/app/src/main/java/com/example/ugri/pathogion/MainActivity.java
@@ -1,34 +1,46 @@
 package com.example.ugri.pathogion;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.ugri.pathogion.Map;
+import com.example.ugri.pathogion.R;
 
 
-public class MainActivity extends FragmentActivity
-    implements OnMapReadyCallback {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //code map to the app
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        Intent intent = new Intent(this, Map.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        //add a marker
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("ME"));
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
-
-
 }
