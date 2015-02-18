759b53c6e5c0135944bf0a1159da32d741167dea
linghongren <lhng.rn@gmail.com>
2015-02-18 16:43:18 -0500

    Display google map by adding code to the main activity

--- pathogion/app/src/main/java/com/example/ugri/pathogion/MainActivity.java
+++ pathogion/app/src/main/java/com/example/ugri/pathogion/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.ugri.pathogion;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity
+    implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //code map to the app
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        //add a marker
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("ME"));
+    }
+
+
 }
